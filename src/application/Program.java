eaf3b05c0cab452d28a7984b423dba7408da9f46
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-04-27 12:54:04 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,7 +11,11 @@ public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
+
         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(),3000.00, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
